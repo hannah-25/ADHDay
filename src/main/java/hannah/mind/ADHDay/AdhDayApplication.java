01692fa122d1cb37eadf414428c4486e3e58a400
hannah <judyjjj106@gmail.com>
2025-06-25 16:51:53 +0900

    Initial commit

---
+++ src/main/java/hannah/mind/ADHDay/AdhDayApplication.java
@@ -0,0 +1,13 @@
+package hannah.mind.ADHDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdhDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdhDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hannah/mind/ADHDay/AdhDayApplicationTests.java
@@ -0,0 +1,13 @@
+package hannah.mind.ADHDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdhDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
